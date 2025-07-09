f350f85006caa368b89487ce39ea4984ad113c96
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:59:53 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "DangerousWorld_2.0" `
+  -ModName "Dangerous World 2.0" `
+  -ModFolder "DangerousWorld" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/dangerous-world/DangerousWorld-2.0-Original.zip" `
+  -ModBaseFilesUrlHash "0551f079ebbd3693653ef435dd2939a37f09c3f2b386704af44999244be27e9e" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
